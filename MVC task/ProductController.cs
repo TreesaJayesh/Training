d7c8532891257a2a9ee7f76f2bed23343d7f1103
TreesaJayesh <145721308+TreesaJayesh@users.noreply.github.com>
2023-10-05 23:05:41 +0530

    Add files via upload

---
+++ MVC task/ProductController.cs
@@ -0,0 +1,85 @@
+﻿using ABC.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ABC.Models;
+using System.Web.Optimization;
+
+namespace ABC.Controllers
+{
+    public class ProductController : Controller
+    {
+        DAL DAL = new DAL();  
+        // GET: Product
+        public ActionResult List()
+        {
+            var data = DAL.GetUsers();
+            return View(data);
+        }
+        public ActionResult Create()
+        {
+           
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Productlist productlist)
+        {
+
+            if (DAL.InsertUser(productlist))
+            {
+                TempData["InsertMsg"] = "User saved successful";
+                return RedirectToAction("List");
+            }
+            else 
+            {
+                TempData["InsertErrorMsg"] = "Data not saved";
+            }
+            return View();
+        }
+       public ActionResult Details(int id)
+        {
+
+            var data = DAL.GetUsers().Find(a => a.id == id);
+            return View(data);
+        }
+        public ActionResult Edit(int id)
+        {
+            var data = DAL.GetUsers().Find(a=>a.id== id);
+            return View(data);
+        }
+        [HttpPost]
+        public ActionResult Edit(Productlist productlist)
+        {
+
+            if (DAL.UpdateUser(productlist))
+            {
+                TempData["UpdateMsg"] = "User updated successful";
+                return RedirectToAction("List");
+            }
+            else
+            {
+                TempData["UpdateErrorMsg"] = "Data not updated";
+            }
+            return View();
+        }
+       
+        public ActionResult Delete(int id) 
+        {
+
+
+            int r = DAL.DeleteUser(id);
+                if(r>0)
+               {
+                TempData["DeleteMsg"] = "User deletedsuccessful";
+                return RedirectToAction("List");
+            }
+            else
+            {
+                TempData["DeleteErrorMsg"] = "Data not deleted";
+            }
+            return View();
+        }
+    }
+}
---
+++ MVC task/Productlist.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ABC.Models
+{
+    public class Productlist
+    {
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "Product Name is required")]
+        public String ProductName { get; set; }
+
+        [Required(ErrorMessage = "Category Id is required")]
+        public int CategoryId { get; set; }
+
+        [Required(ErrorMessage = "Category Name is required")]
+        public String CategoryName { get; set; }
+
+    }
+}
